d0eb8dcab802ea694fc3163f4af362637fc30e69
vitorbenjamin <vitor.benjamin7@gmail.com>
2016-09-20 10:47:14 -0300

    geral 2

--- app/Http/Controllers/ChamadoController.php
+++ app/Http/Controllers/ChamadoController.php
@@ -43,7 +43,7 @@ class ChamadoController extends Controller
         
         $json = json_decode($meujson);        
         if ($json != null){
-            return $json;
+            return response()->json($json);
         }else{
             return "Deu Ruim";
         }       
